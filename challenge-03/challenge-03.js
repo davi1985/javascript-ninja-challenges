9b1236ec18b266f3ba38ac3d4abce149d502797e
Davi Silva <davisilvaphoto@gmail.com>
2023-05-08 17:01:58 -0300

    resolve challenge

--- challenge-03/challenge-03.js
+++ challenge-03/challenge-03.js
@@ -1,8 +1,5 @@
-# Desafio da semana #3
-
-```js
 // Declarar uma variável qualquer, que receba um objeto vazio.
-?
+let myVar = {};
 
 /*
 Declarar uma variável `pessoa`, que receba suas informações pessoais.
@@ -16,14 +13,25 @@ As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
 - `andando` - Boolean - recebe "falso" por padrão
 - `caminhouQuantosMetros` - Number - recebe "zero" por padrão
 */
-?
+let person = {
+  name: "Davi",
+  lastName: "Silva",
+  gender: "M",
+  height: 1.6,
+  age: 37,
+  weight: 90,
+  walking: false,
+  howManyMetersWalking: 0,
+};
 
 /*
 Adicione um método ao objeto `pessoa` chamado `fazerAniversario`. O método deve
 alterar o valor da propriedade `idade` dessa pessoa, somando `1` a cada vez que
 for chamado.
 */
-?
+person.makeBirthDay = function () {
+  return (person.age += 1);
+};
 
 /*
 Adicione um método ao objeto `pessoa` chamado `andar`, que terá as seguintes
@@ -35,103 +43,120 @@ valor dessa propriedade a quantidade passada por parâmetro;
 - Ele deverá modificar o valor da propriedade `andando` para o valor
 booleano que representa "verdadeiro";
 */
-?
+person.walk = function (metersWalkings) {
+  person.walking = true;
+  person.howManyMetersWalking += metersWalkings;
+};
 
 /*
 Adicione um método ao objeto `pessoa` chamado `parar`, que irá modificar o valor
 da propriedade `andando` para o valor booleano que representa "falso".
 */
-?
+person.stop = function () {
+  person.walking = false;
+};
 
 /*
 Crie um método chamado `nomeCompleto`, que retorne a frase:
 - "Olá! Meu nome é [NOME] [SOBRENOME]!"
 */
-?
+person.fullName = function () {
+  return `Olá, Meu nome é ${person.name} ${person.lastName}`;
+};
 
 /*
 Crie um método chamado `mostrarIdade`, que retorne a frase:
 - "Olá, eu tenho [IDADE] anos!"
 */
-?
+person.showAge = function () {
+  return `Olá, eu tenho ${person.age} anos!`;
+};
 
 /*
 Crie um método chamado `mostrarPeso`, que retorne a frase:
 - "Eu peso [PESO]Kg."
 */
-?
+person.showWeight = function () {
+  return `Eu peso ${person.weight}Kg.`;
+};
 
 /*
 Crie um método chamado `mostrarAltura` que retorne a frase:
 - "Minha altura é [ALTURA]m."
 */
-?
+person.showHeight = function () {
+  return `Minha altura é ${person.height}m.`;
+};
 
 /*
 Agora vamos brincar um pouco com o objeto criado:
 Qual o nome completo da pessoa? (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+console.log(person.fullName());
 
 /*
 Qual a idade da pessoa? (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+console.log(person.showAge());
 
 /*
 Qual o peso da pessoa? (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+console.log(person.showWeight());
 
 /*
 Qual a altura da pessoa? (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+console.log(person.showHeight());
 
 /*
 Faça a `pessoa` fazer 3 aniversários.
 */
-?
+person.makeBirthDay();
+person.makeBirthDay();
+person.makeBirthDay();
 
 /*
 Quantos anos a `pessoa` tem agora? (Use a instrução para responder e
 comentários inline ao lado da instrução para mostrar qual foi a resposta
 retornada)
 */
-?
+console.log(person.showAge());
 
 /*
 Agora, faça a `pessoa` caminhar alguns metros, invocando o método `andar` 3x,
 com metragens diferentes passadas por parâmetro.
 */
-?
+person.walk(300);
+person.walk(250);
+person.walk(700);
 
 /*
 A pessoa ainda está andando? (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
-?
+console.log(person.walking);
 
 /*
 Se a pessoa ainda está andando, faça-a parar.
 */
-?
+person.stop();
 
 /*
 E agora: a pessoa ainda está andando? (Use uma instrução para responder e
 comentários inline ao lado da instrução para mostrar a resposta retornada)
 */
-?
+console.log(person.walking);
 
 /*
 Quantos metros a pessoa andou? (Use uma instrução para responder e comentários
 inline ao lado da instrução para mostrar a resposta retornada)
 */
-?
+console.log(person.howManyMetersWalking);
 
 /*
 Agora vamos deixar a brincadeira um pouco mais divertida! :D
@@ -150,8 +175,13 @@ deve conter no retorno da frase acima é "metro" no lugar de "metros".
 método), que será concatenada com a frase de retorno, mostrando a resposta
 correta, de acordo com os dados inseridos no objeto.
 */
-?
+person.intro = function () {
+  let gender = person.gender === "M" ? "o" : "a";
+  let agePlural = person.age > 1 ? "anos" : "ano";
+  let meterPlural = person.howManyMetersWalking > 1 ? "metros" : "metro";
+
+  return `"Olá, eu sou ${gender} ${person.name} ${person.lastName}, tenho ${person.age} ${agePlural}, ${person.height}m, meu peso é ${person.weight}kg e, só hoje, eu já caminhei ${person.howManyMetersWalking} ${meterPlural}!"`;
+};
 
 // Agora, apresente-se ;)
-?
-```
+console.log(person.intro());
--- challenge-04/challenge-04.js
+++ challenge-04/challenge-04.js
@@ -1,21 +1,28 @@
-# Desafio da semana #4
-
-```js
 /*
 Declare uma variável chamada `isTruthy`, e atribua a ela uma função que recebe
 um único parâmetro como argumento. Essa função deve retornar `true` se o
 equivalente booleano para o valor passado no argumento for `true`, ou `false`
 para o contrário.
 */
-?
+let isTruthy = (arg) => (arg ? true : false);
 
 // Invoque a função criada acima, passando todos os tipos de valores `falsy`.
-?
-
+console.log(isTruthy(0));
+console.log(isTruthy(-0));
+console.log(isTruthy(""));
+console.log(isTruthy(undefined));
+console.log(isTruthy(NaN));
+console.log(isTruthy(null));
 /*
 Invoque a função criada acima passando como parâmetro 10 valores `truthy`.
 */
-?
+console.log(isTruthy(1));
+console.log(isTruthy(() => {}));
+console.log(isTruthy({}));
+console.log(isTruthy([]));
+console.log(isTruthy("some text"));
+console.log(isTruthy({ key: "value" }));
+console.log(isTruthy([[], {}]));
 
 /*
 Declare uma variável chamada `carro`, atribuindo à ela um objeto com as
@@ -29,35 +36,42 @@ seguintes propriedades (os valores devem ser do tipo mostrado abaixo):
 - `assentos` - Number - cinco por padrão
 - `quantidadePessoas` - Number - zero por padrão
 */
-?
-
+let car = {
+  brand: "Chevrolet",
+  model: "Agile",
+  licensePlate: "JSX 123",
+  year: 2011,
+  color: "gray",
+  doorsQuantity: 4,
+  seats: 5,
+  personsInCar: 0,
+};
 /*
 Crie um método chamado `mudarCor` que mude a cor do carro conforme a cor
 passado por parâmetro.
 */
-?
-
+car.setColor = (newColor) => {
+  car.color = newColor;
+};
 /*
 Crie um método chamado `obterCor`, que retorne a cor do carro.
 */
-?
+car.getColor = () => car.color;
 
 /*
 Crie um método chamado `obterModelo` que retorne o modelo do carro.
 */
-?
-
+car.getModel = () => car.model;
 /*
 Crie um método chamado `obterMarca` que retorne a marca do carro.
 */
-?
-
+car.getBrand = () => car.brand;
 /*
 Crie um método chamado `obterMarcaModelo`, que retorne:
 "Esse carro é um [MARCA] [MODELO]"
 Para retornar os valores de marca e modelo, utilize os métodos criados.
 */
-?
+car.getBrandAndModel = () => `Esse carro é um ${car.brand} ${car.model}`;
 
 /*
 Crie um método que irá adicionar pessoas no carro. Esse método terá as
@@ -75,48 +89,63 @@ mostrar quantos assentos ainda podem ser ocupados, com a frase:
 - Se couber somente mais uma pessoa, mostrar a palavra "pessoa" no retorno
 citado acima, no lugar de "pessoas".
 */
-?
+car.addPersonInCar = (quantity) => {
+  if (!quantity) return null;
 
+  let allPersons = car.personsInCar + quantity;
+  let howManyPersonIsPossible = car.seats - car.personsInCar;
+  let pluralOuSingular = howManyPersonIsPossible === 1 ? "pessoa" : "pessoas";
+
+  if (car.personsInCar === car.seats && allPersons >= car.seats) {
+    return "O carro já está lotado";
+  }
+
+  if (allPersons > car.seats) {
+    return `Só cabem mais ${howManyPersonIsPossible} ${pluralOuSingular} !`;
+  }
+
+  car.personsInCar += quantity;
+
+  return `Já temos ${car.personsInCar} pessoas no carros`;
+};
 /*
 Agora vamos verificar algumas informações do carro. Para as respostas abaixo,
 utilize sempre o formato de invocação do método (ou chamada da propriedade),
 adicionando comentários _inline_ ao lado com o valor retornado, se o método
 retornar algum valor.
-
-Qual a cor atual do carro?
 */
-?
+// Qual a cor atual do carro?
+console.log(car.getColor());
 
 // Mude a cor do carro para vermelho.
-?
+car.setColor("red");
 
 // E agora, qual a cor do carro?
-?
+console.log(car.getColor());
 
 // Mude a cor do carro para verde musgo.
-?
+car.setColor("green");
 
 // E agora, qual a cor do carro?
-?
+console.log(car.getColor());
 
 // Qual a marca e modelo do carro?
-?
+console.log(car.getBrandAndModel());
 
 // Adicione 2 pessoas no carro.
-?
+console.log(car.addPersonInCar(2));
 
 // Adicione mais 4 pessoas no carro.
-?
+console.log(car.addPersonInCar(4));
 
 // Faça o carro encher.
-?
+console.log(car.addPersonInCar(3));
 
 // Tire 4 pessoas do carro.
-?
+console.log(car.addPersonInCar(-4));
 
 // Adicione 10 pessoas no carro.
-?
+console.log(car.addPersonInCar(10));
 
 // Quantas pessoas temos no carro?
-?
-```
+console.log(car.personsInCar);
