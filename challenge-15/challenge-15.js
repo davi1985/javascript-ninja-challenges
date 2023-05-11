a33309aaed2d8bfe62fc45ed9180b632c4004b9f
Davi Silva <davisilvaphoto@gmail.com>
2023-05-11 10:41:19 -0300

    challenges 15, 16, 17

--- challenge-15/challenge-15.js
+++ challenge-15/challenge-15.js
@@ -2,8 +2,8 @@
 Envolva todo o código desse desafio em uma IIFE.
 Crie um arquivo chamado index.html e adicione esse script ao HTML.
 */
-
-/*
+(() => {
+  /*
 Crie uma função construtora chamada "Person". Esse construtor deve ter
 as seguintes características:
 - Deve receber 3 parâmetros: `name`, `lastName` e `age`;
@@ -21,34 +21,65 @@ as seguintes características:
   adicionados à idade original (age). Esse método deverá retornar o objeto
   que será instanciado.
 */
-// ?
+  function Person(name, lastName, age) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
 
-/*
+    this.getFullName = () => `${this.name} ${this.lastName}`;
+    this.getAge = () => this.age;
+    this.addAge = (newAge) => {
+      this.age += newAge;
+    };
+  }
+
+  /*
 Crie 3 novos objetos usando o construtor acima. Os objetos serão novas
 pessoas. As variáveis deverão ser o primeiro nome da pessoa. Passe os
 parâmetros corretamente para o construtor para criar as novas pessoas.
 Mostre as 3 novas pessoas criadas no console (Um console.log por pessoa).
 */
-console.log( 'Novas pessoas criadas à partir de Person:' );
-// ?
+  console.log("Novas pessoas criadas à partir de Person:");
+  const personOne = new Person("steve", "vai", 54);
+  const personTwo = new Person("joe", "satriani", 56);
+  const personThree = new Person("jimmy", "hendrix", 35);
 
-/*
+  console.log({ personOne, personTwo, personThree });
+
+  /*
 Mostre no console o nome completo de cada pessoa.
 */
-console.log( '\nNomes das pessoas:' );
-// ?
+  console.log("\nNomes das pessoas:");
+  console.log(personOne.getFullName());
+  console.log(personTwo.getFullName());
+  console.log(personThree.getFullName());
 
-/*
+  /*
 Mostre no console as idades de cada pessoa, com a frase:
 - "[NOME COMPLETO] tem [IDADE] anos."
 */
-console.log( '\nIdade das pessoas:' );
-// ?
+  console.log("\nIdade das pessoas:");
+  console.log(`${personOne.getFullName()} tem ${personOne.getAge()} anos`);
+  console.log(`${personTwo.getFullName()} tem ${personTwo.getAge()} anos`);
+  console.log(`${personThree.getFullName()} tem ${personThree.getAge()} anos`);
 
-/*
+  /*
 Adicione alguns anos à cada pessoa, e mostre no console a nova idade de
 cada um. A frase deverá ser no formato:
 - "[NOME COMPLETO] agora tem [NOVA IDADE] anos."
 */
-console.log( '\nNova idade das pessoas:' );
-// ?
+  console.log("\nNova idade das pessoas:");
+  personOne.addAge(2);
+  personTwo.addAge(3);
+  personThree.addAge(4);
+
+  console.log(
+    `${personOne.getFullName()} agora tem ${personOne.getAge()} anos`
+  );
+  console.log(
+    `${personTwo.getFullName()} agora tem ${personTwo.getAge()} anos`
+  );
+  console.log(
+    `${personThree.getFullName()} agora tem ${personThree.getAge()} anos`
+  );
+})();
--- challenge-16/challenge-16.js
+++ challenge-16/challenge-16.js
@@ -3,8 +3,9 @@
 2. Adicione a diretiva 'use strict';
 3. Crie um arquivo index.html e adicione esse script à ele.
 */
-
-/*
+(() => {
+  "use strict";
+  /*
 Declare uma variável chamada `name` que receba seu primeiro nome.
 Mostre no console todas as letras do seu nome separadas, com a frase:
 - "[LETRA] é a [POSIÇÃO]ª letra do meu nome."
@@ -13,10 +14,14 @@ Ex: no caso do nome ser "Fernando", deve mostrar as frases:
 - "e é a 2ª letra do meu nome."
 E assim por diante, até a última.
 */
-console.log( 'As letras do seu nome:' );
-// ?
+  console.log("As letras do seu nome:");
+  const name = "Davi";
 
-/*
+  for (let i = 0; i < name.length; i++) {
+    console.log(`${name[i]} é a ${i + 1}ª letra do meu nome.`);
+  }
+
+  /*
 - Declare uma variável chamada `fullName`, que receba seu nome completo,
 escrito no formato de slug (caixa baixa e palavras separadas por um traço).
 Ex: o nome "Fernando Daciuk" ficaria "fernando-daciuk"
@@ -28,10 +33,17 @@ curso para fazer isso funcionar corretamente :)
 - Mostre no console o nome no formato slug, e o resultado final. Use um
 console.log para cada formato.
 */
-console.log( '\nNome convertido à partir de um slug:' );
-// ?
+  console.log("\nNome convertido à partir de um slug:");
+  const fullName = "davi-silva";
 
-/*
+  const slug = fullName
+    .split("-")
+    .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
+    .join(" ");
+
+  console.log(slug);
+
+  /*
 - Crie um array com 5 nomes. Reduza esses nomes a uma única string, separando
 cada nome por vírgula. Entre o penúltimo e o último nome, o separador deve
 ser um "e". No final, concatene com a frase: " são meus amigos.".
@@ -40,24 +52,36 @@ O resultado final deve ficar mais ou menos assim:
 - Detalhe: o código abaixo deve funcionar com um array de qualquer tamanho.
 5 nomes foi somente uma sugestão ;)
 */
-console.log( '\nMeus amigos:' );
-// ?
+  console.log("\nMeus amigos:");
+  const friends = ["Steve", "Joe", "Jimmy", "Jonh", "Brad"];
+  const lastFriend = friends[friends.length - 1];
+  let finalPhase = `${friends.join(", ")} e ${lastFriend} são meus amigos.`;
 
-/*
+  // another solution
+  // finalPhase = friends
+  //   .reduce((acc, current, index) => {
+  //     const separator = friends.length - 1 === index ? " e " : ", ";
+  //     return acc + separator + current;
+  //   })
+  //   .concat(" são meus amigos.");
+
+  console.log(finalPhase);
+
+  /*
 Usando o replace(), faça a string "Roberto" virar "Roberta".
 Mostre o resultado no console.
 */
-console.log( '\nEra "Roberto", agora é:' );
-// ?
+  console.log('\nEra "Roberto", agora é:');
+  console.log("Roberto".replace("to", "ta"));
 
-/*
+  /*
 Mostre no console a parte "nando" da string "Fernando". Use o método que
 faz a busca do final para o início da string.
 */
-console.log( '\nParte de uma string:' );
-// ?
+  console.log("\nParte de uma string:");
+  console.log("Fernando".substring(8, 3));
 
-/*
+  /*
 Declare uma variável chamada `myName`, que receba o seu primeiro nome,
 escrito de forma natural.
 Mostre no console esse nome, mas com as letras intercalando entre maiúsculas
@@ -66,5 +90,13 @@ e minúsculas.
 de qualquer tamanho, escrito de qualquer forma.
 Ex.: Nomes que deveriam funcionar: "Fernando", "RoBertO", "gabriEla", etc.
 */
-console.log( '\nNome com letras intercaladas entre caixa alta e baixa:' );
-// ?
+  console.log("\nNome com letras intercaladas entre caixa alta e baixa:");
+  const myName = "Davi Pereira da Silva"
+    .split("")
+    .map((item, index) =>
+      index % 2 === 0 ? item.toLowerCase() : item.toUpperCase()
+    )
+    .join("");
+
+  console.log(myName);
+})();
